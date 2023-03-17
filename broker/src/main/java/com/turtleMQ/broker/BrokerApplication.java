ad987a3b2d78b6697f5ec3429c3e94decf5d95da
Johan Smit <johansm@bbd.co.za>
2023-03-17 10:37:00 +0200

    Added context

--- broker/src/main/java/com/turtleMQ/broker/BrokerApplication.java
+++ broker/src/main/java/com/turtleMQ/broker/BrokerApplication.java
@@ -2,11 +2,17 @@ package com.turtleMQ.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class BrokerApplication {
 
 	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.turtleMQ.broker");
+        context.refresh();
+
 		SpringApplication.run(BrokerApplication.class, args);
 	}
 
