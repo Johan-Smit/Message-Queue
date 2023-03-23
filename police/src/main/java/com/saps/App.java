6d2e4df65d92ec1c8670828c6c6257d95d7695fc
Janco Venter <jan@bbd.co.za>
2023-03-23 23:05:13 +0200

    Update police service

--- police/src/main/java/com/saps/App.java
+++ police/src/main/java/com/saps/App.java
@@ -2,7 +2,9 @@ package com.saps;
 
 import com.turtleMQ.MessageHandler;
 
+import requests.AccidentResponse;
 import requests.CallCenter;
+import requests.CrimeResponse;
 
 public class App 
 {
@@ -11,5 +13,13 @@ public class App
         MessageHandler messageHandler = new MessageHandler(8008);
         messageHandler.register(new CallCenter());
         messageHandler.start();
+
+        MessageHandler crimeHandler = new MessageHandler(8001);
+        crimeHandler.register(new CrimeResponse());
+        crimeHandler.start();
+
+        MessageHandler accidentHandler = new MessageHandler(8002);
+        accidentHandler.register(new AccidentResponse());
+        accidentHandler.start();
     }
 }
