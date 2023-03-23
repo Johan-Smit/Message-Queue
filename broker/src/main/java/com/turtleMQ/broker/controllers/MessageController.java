ab4de85297193056d12e3e11328b94ef4de74bc2
Janco Venter <jan@bbd.co.za>
2023-03-23 11:56:31 +0200

    Implement nodemanager

--- broker/src/main/java/com/turtleMQ/broker/controllers/MessageController.java
+++ broker/src/main/java/com/turtleMQ/broker/controllers/MessageController.java
@@ -2,38 +2,23 @@ package com.turtleMQ.broker.controllers;
 
 import java.util.List;
 
+import com.turtleMQ.broker.services.NodeManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turtleMQ.broker.entities.Message;
-import com.turtleMQ.broker.repositories.MessageRepository;
 
 @RestController
 class MessageController {
+    @Autowired NodeManagerService nodeManagerService;
 
-    private final MessageRepository repository; // TODO: Replace with NodeManager
-
-    MessageController(MessageRepository repository) {
-        // TODO: instantiate NodeManager and use instead of repository
-        this.repository = repository;
-    }
-
-    @GetMapping("/messages")
-    List<Message> all() {
-        return repository.findAll();
-    }
+    MessageController() { }
 
     @PostMapping("/messages")
-    Message newMessage(@RequestBody Message newMessage) {
-        try {
-            // TODO: Pass to NodeManager instead of repository
-            return repository.save(newMessage);
-            //return true;
-        } catch (Exception ex) {
-            //return false;
-            throw ex;
-        }
+    void newMessage(@RequestBody Message newMessage) {
+        nodeManagerService.send(newMessage);
     }
 }
